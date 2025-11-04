60facef77382e6771a3d7a21170ed92b664517ef
Psycho499 <ukhaliqansari@gmail.com>
2025-11-04 21:16:02 +0500

    validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -22,6 +22,7 @@ router.post("/signup", async (req, res) => {
             user: { id: user._id, name: user.name, email: user.email }
         });
 
+
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
