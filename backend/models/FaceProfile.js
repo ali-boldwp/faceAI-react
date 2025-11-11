c742a4eeabdf16b1350dcca5f7813f0ebae36bac
ZubairAnser <zubair@devregion.com>
2025-11-11 18:50:41 +0500

    Face Ai done

--- backend/models/FaceProfile.js
+++ backend/models/FaceProfile.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const QuestionSchema = new mongoose.Schema(
   {
     question: { type: String, required: true },
-    answer: { type: String, required: true },
+      answer: [{ type: String, required: true }],
   },
   { _id: false } 
 );
