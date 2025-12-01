af0e60840515fd67a0fe2bd9113c652de7c23401
alira <shabista.techcrew@gmail.com>
2025-12-01 10:45:55 +0500

    Fixes

--- frontend/src/ThemeLayout/index.tsx
+++ frontend/src/ThemeLayout/index.tsx
@@ -1,45 +1,53 @@
-import React, { ReactNode, useState } from "react";
+import React, {ReactNode, useState} from "react";
 import TopBar from "./TopBar";
 import LeftSidebar from "./LeftSidebar";
 import RightSidebar from "Pages/Home/RightSidebar";
+import Image from "../assets/faces/image16.jpg"
 
 interface ThemeLayoutProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
-const ThemeLayout = ({ children }: ThemeLayoutProps) => {
-  const [answers, setAnswers] = useState<{ [key: string]: string }>({});
+const ThemeLayout = ({children}: ThemeLayoutProps) => {
+    const [answers, setAnswers] = useState<{ [key: string]: string }>({});
 
-  const sidebarItems = [
-      { name: "Face Shape" },
-      { name: "Forehead" },
-      { name: "Eyebrows" },
-      { name: "Eyes" },
-      { name: "Nose" },
-      { name: "Cheeks and Cheekbones" },
-      { name: "Mouth and Lips" },
-    { name: "Chin and Jawline" },
-    { name: "Ear (Urechile)" },
-    { name: "Neck and Throat" },
-    { name: "Skin Texture and Facial Wrinkles" },
-  ];
+    const sidebarItems = [
+        {
+            name: "Face Shape",
+            images: {
+                "Round Face": Image,
+                "Oval Face": Image,
+                "Oblong (Long) Face": Image
+            }
+        },
+        {name: "Forehead"},
+        {name: "Eyebrows"},
+        {name: "Eyes"},
+        {name: "Nose"},
+        {name: "Cheeks and Cheekbones"},
+        {name: "Mouth and Lips"},
+        {name: "Chin and Jawline"},
+        {name: "Ear (Urechile)"},
+        {name: "Neck and Throat"},
+        {name: "Skin Texture and Facial Wrinkles"},
+    ];
 
-  const childrenWithProps = React.cloneElement(children as React.ReactElement, {
-    sidebarItems,
-    answers,
-    setAnswers,
-  });
+    const childrenWithProps = React.cloneElement(children as React.ReactElement, {
+        sidebarItems,
+        answers,
+        setAnswers,
+    });
 
-  return (
-    <>
-      <TopBar />
-      <div className="dash-board-main-wrapper">
-        {/* <LeftSidebar sidebarItems={sidebarItems} answers={answers} /> */}
-        {childrenWithProps}
-        {/*<RightSidebar />*/}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <TopBar/>
+            <div className="dash-board-main-wrapper">
+                {/* <LeftSidebar sidebarItems={sidebarItems} answers={answers} /> */}
+                {childrenWithProps}
+                {/*<RightSidebar />*/}
+            </div>
+        </>
+    );
 };
 
 export default ThemeLayout;
